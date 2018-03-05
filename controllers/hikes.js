6fbf85bb72ea602f39d3689f23b480f90add5b8e
Sam Gehret <sam.gehret@gmail.com>
2018-03-05 10:44:58 -0500

    added post endpoint and created form

--- controllers/hikes.js
+++ controllers/hikes.js
@@ -4,17 +4,36 @@ const router = express.Router()
 const Hike = require('../models/Hikes')
 
 router.get('/', (req, res) => {
-  console.log('getting to hike!!!!!')
   Hike.find({})
         .then(hikes => {
-          res.render('hikes/list', {hikes})
+          res.render('hikes/list', { hikes })
         })
 })
 
+router.post('/', (req, res) => {
+  Hike.create({
+    hikeTitle: req.body.hikeTitle,
+    datePosted: Date.now(),
+    author: req.body.author,
+    drivingDistance: req.body.drivingDistance,
+    hikeDuration: req.body.hikeDuration,
+    hikeDifficulty: req.body.hikeDifficulty,
+    hikeContent: req.body.hikeContent
+  })
+    .then(hike => {
+      res.redirect('/hikes')
+    })
+})
+
+router.get('/new', (req, res) => {
+  console.log('NEW HIKE!!!!!')
+  res.render('hikes/new')
+})
+
 router.get('/:id', (req, res) => {
-  Hike.findOne({_id: req.params.id})
+  Hike.findOne({ _id: req.params.id })
         .then(hike => {
-          res.render('hikes/show', {hike})
+          res.render('hikes/show', { hike })
         })
 })
 
