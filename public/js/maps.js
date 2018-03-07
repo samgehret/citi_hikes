037785aa38e1556062b4e488740c90be99e4e205
Sam Gehret <sam.gehret@gmail.com>
2018-03-07 13:47:53 -0500

    updated maps and view, with updated seed data

--- public/js/maps.js
+++ public/js/maps.js
@@ -5,7 +5,7 @@ function initMap () {
 //   var haight = new google.maps.LatLng(37.7699298, -122.4469157)
 //   var oceanBeach = new google.maps.LatLng(37.7683909618184, -122.51089453697205)
   var mapOptions = {
-    zoom: 14,
+    // zoom: 14,
     center: new google.maps.LatLng(38.9014847, -77.0391641)
   }
   var map = new google.maps.Map(document.getElementById('map'), mapOptions)
@@ -13,7 +13,7 @@ function initMap () {
   var selectedMode = 'DRIVING'
   var request = {
     origin: 'Washington, DC',
-    destination: 'Old Rag Mountain',
+    destination: document.getElementById('title').innerHTML,
           // Note that Javascript allows us to access the constant
           // using square brackets and a string value as its
           // "property."
