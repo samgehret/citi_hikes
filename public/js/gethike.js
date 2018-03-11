713203e73f0f981a5cbbfe70eaa9782d5decb7a7
Sam Gehret <sam.gehret@gmail.com>
2018-03-11 15:44:25 -0400

    polished map and added all hikes map

--- public/js/gethike.js
+++ public/js/gethike.js
@@ -17,11 +17,15 @@ fetch(url)
   })
 
 function populateHike (response) {
-  console.log(response)
   document.querySelector('.title').innerHTML = response.trails[0].name
-  document.getElementById('summary').innerHTML = response.trails[0].summary
-  console.log(lat)
-  console.log(long)
+//   document.getElementById('summary').innerHTML = response.trails[0].summary
+  document.querySelector('.length').innerHTML = response.trails[0].length
+  document.querySelector('.difficulty').innerHTML = response.trails[0].difficulty
+  document.querySelector('.status').innerHTML = response.trails[0].conditionStatus
+  document.querySelector('.statusdetails').innerHTML = response.trails[0].conditionDetails
+  document.querySelector('.ascent').innerHTML = response.trails[0].ascent
+  lat = response.trails[0].latitude
+  lat = response.trails[0].latitude
   lat = response.trails[0].latitude
   long = response.trails[0].longitude
   getRoute()
@@ -36,17 +40,18 @@ function getRoute () {
   var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10',
-    center: [-77.01, 38.89],
-    zoom: 6
+    center: [((-77.01 + long) / 2), ((38.89 + lat) / 2)],
+    zoom: 7
   })
   var start = [-77.01, 38.89]
   var end = [long, lat]
-  var directionsRequest = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?geometries=geojson&access_token=' + 'pk.eyJ1Ijoic2FtZ2VocmV0IiwiYSI6ImNqZWExcDdwNTAxYnEyeG1tZnQ4MTNsODkifQ.68r_UjBeRkubf5eUs4uw-g'
+  var directionsRequest = 'https://api.mapbox.com/directions/v5/mapbox/driving/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?steps=true&geometries=geojson&access_token=' + 'pk.eyJ1Ijoic2FtZ2VocmV0IiwiYSI6ImNqZWExcDdwNTAxYnEyeG1tZnQ4MTNsODkifQ.68r_UjBeRkubf5eUs4uw-g'
   $.ajax({
     method: 'GET',
     url: directionsRequest
   }).done(function (data) {
     var route = data.routes[0].geometry
+    console.log(data)
     map.addLayer({
       id: 'route',
       type: 'line',
@@ -58,9 +63,41 @@ function getRoute () {
         }
       },
       paint: {
-        'line-width': 2
+        'line-width': 2,
+        'line-color': 'red'
       }
     })
+    map.addLayer({
+      id: 'start',
+      type: 'circle',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: start
+          }
+        }
+      }
+    })
+    map.addLayer({
+      id: 'end',
+      type: 'circle',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: end
+          }
+        }
+      }
+    })
+    var distance = (data.routes[0].distance / 1609.344).toFixed(0)
+    document.querySelector('.distance').innerHTML = distance
+    console.log(distance)
             // this is where the code from the next step will go
   })
 }
--- public/js/maps.js
+++ public/js/maps.js
@@ -8,13 +8,13 @@ var trailResponse = ''
 // For now all we have to pass fetch is the url
 
 function doSomething (response) {
-  console.log(response.trails)
   for (let trail of response.trails) {
-    console.log(trail.name)
+    // console.log(trail.name)
     var hikeCard = document.createElement('div')
     hikeCard.className = 'hikecard'
     var hikeTitle = document.createElement('p')
     var hikeLink = document.createElement('a')
+    hikeLink.className = 'listlink'
     hikeLink.href = `/hikes/${trail.id}`
     hikeTitle.innerHTML = trail.name
     hikeLink.appendChild(hikeCard)
@@ -24,52 +24,50 @@ function doSomething (response) {
   // document.querySelector('.hiketitle').innerHTML = response.trails[0].name
 }
 
-fetch(url)
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtZ2VocmV0IiwiYSI6ImNqZWExcDdwNTAxYnEyeG1tZnQ4MTNsODkifQ.68r_UjBeRkubf5eUs4uw-g'
+var map = new mapboxgl.Map({
+  container: 'mapall',
+  style: 'mapbox://styles/mapbox/streets-v10',
+  center: [-77.01, 38.89],
+  zoom: 6
+})
+map.on('load', function () {
+  getRoute()
+})
+
+function getRoute () {
+  fetch(url)
   .then((res) => {
     return res.json()
   })
   .then((res) => {
     // console.log('success!', res)
     doSomething(res)
+    printRoutes(res)
   })
   .catch((err) => {
     console.log('something went wrong...', err)
   })
+}
 
-// mapboxgl.accessToken = 'pk.eyJ1Ijoic2FtZ2VocmV0IiwiYSI6ImNqZWExcDdwNTAxYnEyeG1tZnQ4MTNsODkifQ.68r_UjBeRkubf5eUs4uw-g'
-// var map = new mapboxgl.Map({
-//   container: 'map',
-//   style: 'mapbox://styles/mapbox/streets-v10',
-//   center: [-84.524, 39.118],
-//   zoom: 12
-// })
-// map.on('load', function () {
-//   getRoute()
-// })
-
-// function getRoute () {
-//   var start = [-84.518641, 39.134270]
-//   var end = [-84.512023, 39.102779]
-//   var directionsRequest = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?geometries=geojson&access_token=' + 'pk.eyJ1Ijoic2FtZ2VocmV0IiwiYSI6ImNqZWExcDdwNTAxYnEyeG1tZnQ4MTNsODkifQ.68r_UjBeRkubf5eUs4uw-g'
-//   $.ajax({
-//     method: 'GET',
-//     url: directionsRequest
-//   }).done(function (data) {
-//     var route = data.routes[0].geometry
-//     map.addLayer({
-//       id: 'route',
-//       type: 'line',
-//       source: {
-//         type: 'geojson',
-//         data: {
-//           type: 'Feature',
-//           geometry: route
-//         }
-//       },
-//       paint: {
-//         'line-width': 2
-//       }
-//     })
-//             // this is where the code from the next step will go
-//   })
-// }
+function printRoutes (response) {
+  var allTrails = response.trails
+  console.log(allTrails)
+  for (let trail of allTrails) {
+    map.addLayer({
+      id: trail.name,
+      type: 'circle',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [trail.longitude, trail.latitude]
+          }
+        }
+      }
+    })
+              // this is where the code from the next step will go
+  }
+}
